0470bc7c9af2efe9a7099c8db3e80bf8a571835f
Matheus <ferreiramatheus48@gmail.com>
2021-05-17 01:49:40 -0300

    Added title and description

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../styles/theme'
-import { Head } from 'next/document'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,18 +5,10 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <title>Nexjs + Chakra Boilerplate</title>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
             rel="stylesheet"
-          />
-          <link rel="shortcut icon" href="/img/icon-512.png" />
-          <link rel="apple-touch-icon" href="/img/icon-512.png" />
-          <link rel="manifest" href="/manifest.json" />
-          <meta
-            name="description"
-            content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
           />
         </Head>
         <body>
